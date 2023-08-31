5f84d10da5eb14d19efdd4e5aa88acc4297edd73
Kr1PtunZ <coo.boy2017@yandex.ru>
2023-08-31 15:15:47 +0500

    task 1

--- task1.php
+++ task1.php
@@ -3,19 +3,18 @@ $i = 1;
 while($i <= 100){
     switch ($i){
         case !is_float($i/3) && !is_float($i/5);
-            echo 'FizzBuzz: ';
+            echo 'FizzBuzz'.'<br>';
             break;
 
         case !is_float($i/3);
-            echo 'Fizz: ';
+            echo 'Fizz'.'<br>';
             break;
 
         case !is_float($i/5);
-            echo 'Buzz: ';
+            echo 'Buzz'.'<br>';
             break;
         case $i;
-            echo $i;
-        break;
+            echo $i .'<br>';
     }
     $i++;
 }
